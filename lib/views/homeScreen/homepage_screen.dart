d9ae5ea70bd30b62226a398e094aaa4318e1823c
binod kandel <annomy634@gmail.com>
2025-04-10 09:09:30 +0545

    updated

---
+++ lib/views/homeScreen/homepage_screen.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../auth_screen/login_screen.dart';
+import 'home_Screen.dart';
+
+class AuthenticatePage extends StatelessWidget {
+  const AuthenticatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text("Something went wrong!"),
+          );
+        } else if (snapshot.hasData) {
+          return const HomeScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
+    );
+  }
+}
--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -1,10 +1,10 @@
 import 'package:ecommerce/consts/consts.dart';
 import 'package:ecommerce/views/auth_screen/login_screen.dart';
 import 'package:ecommerce/views/homeScreen/home.dart';
+import 'package:ecommerce/views/homeScreen/homepage_screen.dart';
 import 'package:ecommerce/widgets_common/applogo_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:get/route_manager.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -16,19 +16,22 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   //cretaing a method to change screen
-  changeScreen() {
-    Future.delayed(Duration(seconds: 3), () {
-      //using get X
-      // Get.to(() => const LoginScreen());
+  changeScreen() async {
+    // Already done this in authentatelogin screen see/check there.
+    await Future.delayed(Duration(seconds: 3));
+    // Future.delayed(Duration(seconds: 3), () {
+    //   //using get X
+    //   // Get.to(() => const LoginScreen());
 
-      auth.authStateChanges().listen((User? user) {
-        if (user == null && mounted) {
-          Get.to(() => LoginScreen());
-        } else {
-          Get.to(() => const Home());
-        }
-      });
-    });
+    //   auth.authStateChanges().listen((User? user) {
+    //     if (user == null && mounted) {
+    //       Get.offAll(() => LoginScreen());
+    //     } else {
+    //       Get.offAll(() => const Home());
+    //     }
+    //   });
+    // });
+    Get.offAll(() => const AuthenticatePage());
   }
 
   @override
